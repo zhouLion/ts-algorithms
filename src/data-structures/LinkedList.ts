8972f4a82d6a227a604675bea3a8277a86a2a266
zhouLion <zhouquan147369@gamil.com>
2022-05-20 00:13:07 +0800

    数据结构

--- src/data-structures/LinkedList.ts
+++ src/data-structures/LinkedList.ts
@@ -3,6 +3,13 @@ export type LinkedNode<T> = {
   next: LinkedNode<T> | null;
 }
 
+export function craeteLinkedNode<T>(value: T): LinkedNode<T> {
+  return {
+    value,
+    next: null,
+  };
+}
+
 /**
  * @description
  * # 链表
@@ -20,189 +27,159 @@ export type LinkedNode<T> = {
  *
  * 更快的访问，如随机访问，是不可行的。与链表相比，数组具有更好的缓存位置。
  */
-export type LinkedList<T> = {
-  head: LinkedNode<T> | null;
-  tail: LinkedNode<T> | null;
-  size: number;
-  /**
-   * 将元素插入到链表的头部
-   * @param value
-   * @description
-   * ```
-   * append(value)
-   *  执行前: value 是要添加到列表的值
-   *  执行后: value 被放置到列表的尾部
-   *  另 n 为 构建的 LinkedListNode(value)
-   *  如果 head 为 空
-   *    另 head 为 n
-   *    且 tail 为 n
-   *  否则
-   *    另 tail.next 为 n
-   *    且 tail 为 n
-   *```
-   */
-  append(value: T): LinkedList<T>;
+export function createLinkedList<T>() {
+  const linkedList = {
+    head: null as LinkedNode<T> | null,
+    tail: null as LinkedNode<T> | null,
 
-  /**
-   * 将元素添加到链表的尾部
-   * @param value
-   * @description
-   * ```
-   * prepend(value)
-      执行前: value 是要添加到列表的值
-      执行后: value 被放置到列表的头部
-      另 n 为 构建的 LinkedListNode(value)
-      且 n.next 为 head
-      且 head 为 n
-      如果 tail 为空
-        另 tail 为 n
-   * ```
-   */
-  prepend(value: T): LinkedList<T>;
-
-  /**
-   * 将元素插入到链表的指定位置
-   * @param value
-   * @param index
-   * @description
-   * ```
-   * insert(value, index)
-   *  执行前: value 是要添加到列表的值
-   *         index 是要添加到列表的位置
-   *  执行后: value 被放置到列表的指定位置
-   *  另 n 为 构建的 LinkedListNode(value)
-   *  如果 index = 0
-   *    另 n.next 为 head
-   *    且 head 为 n
-   *    如果 tail 为空
-   *      另 tail 为 n
-   *  否则
-   *    另 i 为 1
-   *    只要 i < index，就循环
-   *      另 i 为 i + 1
-   *      且 p 为 p.next
-   *    另 n.next 为 p.next
-   *    且 p.next 为 n
-   *    如果 tail 为 p
-   *      另 tail 为 n
-   * ```
-   */
-  insert(value: T, index: number): LinkedList<T>;
-
-  /**
-   * 搜索链表中的元素
-   * @param value
-   * @description
-   * ```
-   * contains(value)
-   *  执行前: value 是要搜索的值
-   *  执行后: 返回值为 true 或 false
-   *  另 n 为 head
-   *  只要 n 不为空且 n.value 不等于 value，就循环
-   *    另 n 为 n.next
-   *  如果 n 为空
-   *    直接返回值 false
-   *  否则
-   *    直接返回值 true
-   * ```
-   */
-  contains(value: T): boolean;
-
-  /**
-   * 移除链表中的元素
-   * @param value
-   * @description
-   * ```
-   * remove(value)
-   *  执行前: value 是要删除的值
-   *  执行后: value 若从链表删除, 返回 true, 否则返回 false
-   *  另 n 为 head
-   *  如果 n.value 为 value
-   *    如果 head = tail
-   *      另 head 为 ø
-   *      且 tail 为 ø
-   *    否则
-   *      另 head 为 head.next
-   *    返回 true
-   *  只要 n.next 不为空 且 n.next.value 不等于 value，就循环
-   *    另 n 为 n.next
-   *  如果 n.next 不为空
-   *    如果 n.next = tail
-   *      另 tail 为 n
-   *    另 n.next 为 n.next.next
-   *    返回 true
-   *  返回 false
-   *```
-   */
-  remove(value: T): boolean;
-
-  /**
-   *
-   * @param head
-   */
-  traverse(head: LinkedNode<T>): T[];
-  reverseTraversal(head: LinkedNode<T>, tail: LinkedNode<T>): T[];
-  toArray(): T[];
-  fromArray(array: T[]): void;
-  toString(): string;
-}
-
-export function craeteLinkedNode<T>(value: T): LinkedNode<T> {
-  return {
-    value,
-    next: null,
-  };
-}
-
-export function createLinkedList<T>(): LinkedList<T>;
-export function createLinkedList<T>(): LinkedList<T> {
-  const linkedList: LinkedList<T> = {
-    head: null,
-    tail: null,
+    /**
+     * 将元素添加到链表的尾部
+     * @param value
+     * @description
+     * ```
+     * prepend(value)
+        执行前: value 是要添加到列表的值
+        执行后: value 被放置到列表的头部
+        另 n 为 构建的 LinkedListNode(value)
+        且 n.next 为 head
+        且 head 为 n
+        如果 tail 为空
+          另 tail 为 n
+     * ```
+     */
     prepend(value: T) {
       const node = craeteLinkedNode(value);
-      const { head, tail } = this
-      node.next = head;
       this.head = node;
-      if (!tail) {
-        this.tail = node;
-      }
+      !this.tail && (this.tail = node);
       return this;
     },
+
+    /**
+     * 将元素插入到链表的头部
+     * @param value
+     * @description
+     * ```
+     * append(value)
+     *  执行前: value 是要添加到列表的值
+     *  执行后: value 被放置到列表的尾部
+     *  另 n 为 构建的 LinkedListNode(value)
+     *  另 head 指向 n
+     *  若 tail 为 null, 则 tail 指向 n
+     *```
+    */
     append(value: T) {
-      debugger
       const node = craeteLinkedNode(value);
-      const { head } = this
-      if (!head) {
+      if (!this.head) {
         this.head = node;
         this.tail = node;
         return this;
       }
+      this.tail && (this.tail.next = node);
       this.tail = node;
-      this.tail.next = node;
       return this;
     },
-    insert: function (value: T, index: number): LinkedList<T> {
-      throw new Error("Function not implemented.");
+
+    /**
+       * 将元素插入到链表的指定位置
+       * @param value
+       * @param index
+       * @description
+       * ```
+       * insert(value, index)
+       *  执行前: value 是要添加到列表的值
+       *         index 是要添加到列表的位置
+       *  执行后: value 被放置到列表的指定位置
+       *  如果 index w为 0
+       *    另 调用 prepend(value)
+       *  否则
+       *    另 n 为 构建的 LinkedListNode(value)
+       *    另 i 为 1
+       *    将 p 指向 head
+       *    只要 p 还有值，就循环
+       *      另 i 递增 1
+       *      且 将 p 指向 p.next
+       *    如果 p 存在
+       *      另 n.next 指向 p.next
+       *      另 p.next 指向 n
+       *   否则
+       *      另 head 指向 n
+       *      且 tail 指向 n
+       * ```
+       */
+    insert: function (value: T, index: number) {
+      index = Math.max(0, index);
+      if (index === 0) {
+        this.prepend(value);
+      } else {
+        const pointer = {
+          index: 1,
+          current: this.head,
+        };
+        const node = craeteLinkedNode(value);
+        while (pointer.current) {
+          if (pointer.index === index) break;
+          pointer.index++;
+          pointer.current = pointer.current.next;
+        }
+        if (pointer.current) {
+          node.next = pointer.current;
+          pointer.current = node;
+        } else {
+          if (this.tail) {
+            this.tail.next = node;
+            this.tail = node;
+          } else {
+            this.head = node;
+            this.tail = node;
+          }
+        }
+      }
+      return this;
     },
-    contains: function (head: LinkedNode<T>, value: T): boolean {
-      throw new Error("Function not implemented.");
+
+    find(value: T) {
+      if (!this.head) return null;
+      let current: LinkedNode<T> | null = this.head;
+      while (current) {
+        if (current.value === value) return current;
+        current = current.next;
+      }
+      return null;
     },
-    remove: function (head: LinkedNode<T>, value: T): boolean {
-      throw new Error("Function not implemented.");
+
+    delete(value: unknown) {
+      if (!this.head) return null;
+      let current = this.head;
+      while (current) {
+        if (!current.next) break;
+        if (current.next.value === value) {
+          current.next = current.next.next;
+          break;
+        }
+        current = current.next;
+      }
+      return this;
     },
-    traverse: function (head: LinkedNode<T>): T[] {
-      throw new Error("Function not implemented.");
+
+    deleteHead() {
+      if (!this.head) return null;
+      this.head = this.head.next;
+      return this;
     },
-    reverseTraversal: function (head: LinkedNode<T>, tail: LinkedNode<T>): T[] {
-      throw new Error("Function not implemented.");
-    },
-    toArray: function (): T[] {
-      throw new Error("Function not implemented.");
-    },
-    fromArray: function (array: T[]): void {
-      throw new Error("Function not implemented.");
+
+    deleteTail() {
+      if (!this.head) return null;
+      if (!this.tail) return null;
+      let current = this.head;
+      while (current.next !== this.tail) {
+        if (!current.next) break;
+
+        current = current.next;
+      }
+      this.tail = current;
+      return this;
     },
   };
+
   return linkedList;
 }
